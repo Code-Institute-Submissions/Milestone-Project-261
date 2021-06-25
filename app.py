e4876a0218a8812eb50a27b5a0880cb260165de0
sanjaysanghera <sanjay.sanghera@gmail.com>
2021-06-25 17:32:23 +0000

    Add Watchlist template

--- app.py
+++ app.py
@@ -88,6 +88,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/watchlist")
+def watchlist():
+    return render_template("watchlist.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
